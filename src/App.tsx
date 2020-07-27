c02a2af92e8323c4c543226f43ed8571993d130b
KasNG <kasngvn@gmail.com>
2020-07-27 23:08:48 +0700

    Init project with reactjs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import {DatePicker} from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker/>
+    </div>
+  );
+}
+
+export default App;
